31b8d5ad8dcfcb3e67a4ed5ffd5b99cb046dfcbe
churin <wl00796865@gmail.com>
2024-08-03 18:09:51 +0800

    feat(core): add data preview layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import "./App.css";
 import ImagePanelLayout from "./components/ImageUpload/ImagePanelLayout";
+import DataPreviewLayout from "./components/DataPreview/DataPreviewLayout";
 
 export function App() {
-  return <ImagePanelLayout />;
+  return (
+    <div className="app-container">
+      <ImagePanelLayout />
+      <DataPreviewLayout />
+    </div>
+  );
 }
---
+++ src/components/DataPreview/DataPreviewLayout.tsx
@@ -0,0 +1,7 @@
+import "./DataPreviewLayout.css";
+
+const DataPreviewLayout = () => {
+  return <div className="data-preview-container"></div>;
+};
+
+export default DataPreviewLayout;
