7fb845d1386182ebcd347359462e77396a3dfc0f
churin <wl00796865@gmail.com>
2024-08-03 17:52:23 +0800

    feat(core): add image uploader layout

--- src/components/ImageUpload/ImagePanelLayout.tsx
+++ src/components/ImageUpload/ImagePanelLayout.tsx
@@ -1,10 +1,14 @@
 import "./ImagePanelLayout.css";
+import ImageUploader from "./ImageUploader";
 
 const ImagePanelLayout = () => {
   return (
     <div className="container">
       <div className="navbar">
         <div className="circle" />
+      </div>
+      <div className="control-container">
+        <ImageUploader />
       </div>
     </div>
   );
---
+++ src/components/ImageUpload/ImageUploader.tsx
@@ -0,0 +1,11 @@
+import "./ImageUploader.css";
+
+const ImageUploader = () => {
+  return (
+    <div className="dropzone-area">
+      <p className="upload-text">Upload image</p>
+    </div>
+  );
+};
+
+export default ImageUploader;
